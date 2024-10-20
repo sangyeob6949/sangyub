df5167253e102a7ce877596204bf5d05134db8d2
sangyeob6949 <163505589+sangyeob6949@users.noreply.github.com>
2024-10-20 18:52:20 +0900

    Delete 6주차.py

--- 6주차.py
+++
@@ -1,6 +0,0 @@
-gugudan = int(input("몇 단까지 출력 할까요?:"))
-
-for gugu in range(1,gugudan+1):
-    print("------[",gugu,"단]------ 입니다")
-    for dan in range(1,20):
-        print(gugu,"x",dan,"=",gugu*dan)
